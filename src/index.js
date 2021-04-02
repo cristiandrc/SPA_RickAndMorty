792861b3b8e7541e326aeb1a2c0b712f3f499e87
cristian <crisdarroca@hotmail.com>
2021-04-01 20:33:56 -0500

    gp

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './sass/style.scss'
-import Router from './routes/router'
-
-
-
-window.addEventListener('load', Router())
-window.addEventListener('hashchange', Router)
--- src/pages/Error404.js
+++
@@ -1,6 +0,0 @@
-
-const Error404 = () => {
-    console.log('Error 404 ')
-}
-
-export default Error404
