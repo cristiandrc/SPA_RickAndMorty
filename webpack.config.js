e239b23b8be83d8036266144d8c425e60e05c39c
cristian <crisdarroca@hotmail.com>
2021-05-24 20:49:32 -0500

    Cambios

--- webpack.config.js
+++ webpack.config.js
@@ -70,7 +70,7 @@ module.exports = {
 
 
     devServer: {
-        contentBase: path.join(__dirname, 'dist'),
+        contentBase: path.join(__dirname, 'docs'),
         compress: true,
         historyApiFallback: true,
         port: 8080,
